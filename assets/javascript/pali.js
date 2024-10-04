4acfd36eb3091e2b055da08d4334125af553a260
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-04 17:25:25 +0200

    add while loop

--- assets/javascript/pali.js
+++ assets/javascript/pali.js
@@ -2,28 +2,31 @@
 Palidroma
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 */
-
-
-let wordToCheck = prompt('inserisci la parola da verificare');
-let wordToCheckLC = wordToCheck.toLowerCase()
-let reversed = ""
-let reversedLowerCase
-
-function checkWord (wordToCheckLC){
-
-for (let i = wordToCheckLC.length - 1; i >= 0; i--) {
-    reversed = reversed + wordToCheckLC[i];
-    reversedLowerCase = reversed.toLowerCase() 
+let AnOtherWord = true;
+while (AnOtherWord){
+    let wordToCheck = prompt('inserisci la parola da verificare');
+    let wordToCheckLC = wordToCheck.toLowerCase()
+    let reversed = ""
+    let reversedLowerCase
+    
+    function checkWord (wordToCheckLC){
+    
+    for (let i = wordToCheckLC.length - 1; i >= 0; i--) {
+        reversed = reversed + wordToCheckLC[i];
+        reversedLowerCase = reversed.toLowerCase() 
+    }
+    if (wordToCheckLC == reversedLowerCase) {
+       return (`${wordToCheckLC} is palindrome`);
+    
+    }else {
+        return (`${wordToCheckLC} is not palindrome`);        
+    }
+    }
+    
+    alert(checkWord(wordToCheckLC));
+    
+AnOtherWord = confirm ('Wanna ask an other word?')
 }
-if (wordToCheckLC == reversedLowerCase) {
-   return (`${wordToCheckLC} is palindrome`);
-
-}else {
-    return (`${wordToCheckLC} is not palindrome`);        
-}
-}
-
-console.log(checkWord(wordToCheckLC));
 
 
 
