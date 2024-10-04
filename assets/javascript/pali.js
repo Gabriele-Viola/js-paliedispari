3f830f0154409c8c12fe2c3a22d8d58c47f4f7c2
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-10-04 15:30:22 +0200

    add for loop

--- assets/javascript/pali.js
+++ assets/javascript/pali.js
@@ -1,1 +1,32 @@
+/*
+Palidroma
+Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+*/
 
+
+let wordToCheck = "radar";
+let reversed = "";
+let message
+
+
+function ChekWord(wordToCheck) {
+    let wordToCheck = "radar";
+    let reversed = "";
+    
+
+    for (let i = wordToCheck.length - 1; i >= 0; i--) {
+        reversed += wordToCheck[i];
+
+
+    }
+    if (wordToCheck == reversed) {
+        message =`${wordToCheck} is palindrome`;
+        return message;
+
+    } else {
+        message = `${wordToCheck} is NOT palindrome`;
+        return message
+    }
+
+}
+console.log(message);
